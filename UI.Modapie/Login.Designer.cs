9ca433c9ee94d107afff49d802db064158dae0d3
ricar <ricar@DESKTOP-EHDAC3S>
2018-07-03 15:38:38 -0600

    h

--- UI.Modapie/Login.Designer.cs
+++ UI.Modapie/Login.Designer.cs
@@ -18,6 +18,7 @@
                 components.Dispose();
             }
             base.Dispose(disposing);
+            //hhhhhh
         }
 
         #region Código generado por el Diseñador de Windows Forms
