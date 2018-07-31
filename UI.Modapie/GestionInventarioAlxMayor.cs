72b1a276a01bcc624908513413321a7640c6434b
LuisD <l_diegojimenez@hotmail.com>
2018-07-31 15:09:05 -0600

    Validacion Campo IdLote

--- UI.Modapie/GestionInventarioAlxMayor.cs
+++ UI.Modapie/GestionInventarioAlxMayor.cs
@@ -26,35 +26,40 @@ namespace UI.Modapie
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            try
-            {                
-              ProductosAlxMayor prod = Mantenimiento.Instancia.obtenerLote(Convert.ToInt32(txtLote.Text));
-
-                txtOrdenEnvio.Text = prod.EnvioOrden1.ToString();
-                txtShow.Text = prod.Show1;
-                txtCodigo.Text = prod.Codigo1.ToString();
-                txtCliente.Text = prod.Cliente1;
-                txtVendedor.Text = Convert.ToString(prod.Vendedor1);
-                txtZona.Text = Convert.ToString(prod.Zona1);
-                txtTransporte.Text = Convert.ToString(prod.Transporte1);
-                txtPedido.Text = prod.Pedido1;
-                txtModelo.Text = Convert.ToString(prod.Modelo1);
-                txtColor.Text = Convert.ToString(prod.Color1);
-                rtbDescripcion.Text = prod.Descripcion1;
-                txtCurva.Text = prod.Curva1;
-                txtPares.Text = Convert.ToString(prod.Pares1);
-                txtCajas.Text = Convert.ToString(prod.Cajas1);
-                txtCif.Text = Convert.ToString(prod.Cif1);
-                txtTotalCif.Text = Convert.ToString(prod.TotalCIF1);
-                txtPrecioUnitario.Text = prod.PrecioUnitario1.ToString();
-                txtSubTotal.Text = Convert.ToString(prod.SubTotal1);
-                txtTotalImpuestos.Text = Convert.ToString(prod.TotalImpuestos1);
-            }
-            catch (Exception ee)
+            if (!txtLote.Text.Equals(""))
             {
+                try
+                {
+                    ProductosAlxMayor prod = Mantenimiento.Instancia.obtenerLote(Convert.ToInt32(txtLote.Text));
 
-                DialogResult d = MessageBox.Show(ee.Message.ToString(), "Error");
+                    txtOrdenEnvio.Text = prod.EnvioOrden1.ToString();
+                    txtShow.Text = prod.Show1;
+                    txtCodigo.Text = prod.Codigo1.ToString();
+                    txtCliente.Text = prod.Cliente1;
+                    txtVendedor.Text = Convert.ToString(prod.Vendedor1);
+                    txtZona.Text = Convert.ToString(prod.Zona1);
+                    txtTransporte.Text = Convert.ToString(prod.Transporte1);
+                    txtPedido.Text = prod.Pedido1;
+                    txtModelo.Text = Convert.ToString(prod.Modelo1);
+                    txtColor.Text = Convert.ToString(prod.Color1);
+                    rtbDescripcion.Text = prod.Descripcion1;
+                    txtCurva.Text = prod.Curva1;
+                    txtPares.Text = Convert.ToString(prod.Pares1);
+                    txtCajas.Text = Convert.ToString(prod.Cajas1);
+                    txtCif.Text = Convert.ToString(prod.Cif1);
+                    txtTotalCif.Text = Convert.ToString(prod.TotalCIF1);
+                    txtPrecioUnitario.Text = prod.PrecioUnitario1.ToString();
+                    txtSubTotal.Text = Convert.ToString(prod.SubTotal1);
+                    txtTotalImpuestos.Text = Convert.ToString(prod.TotalImpuestos1);
+                }
+                catch (Exception ee)
+                {
+
+                    DialogResult d = MessageBox.Show(ee.Message.ToString(), "Error");
+                }
             }
+            else
+                MessageBox.Show("El campo IdLote se encuentra en blanco", "Error", MessageBoxButtons.OK,MessageBoxIcon.Warning);
         }
 
         private void btnEliminar_Click(object sender, EventArgs e)
