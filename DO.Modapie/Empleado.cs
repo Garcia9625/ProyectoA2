dd2d4743827f2599f7484d81f5c9fe7ea5f9467a
ricar <ricar@DESKTOP-EHDAC3S>
2018-08-17 21:41:51 -0600

    DoDescripcionYVentaalxMayor

--- DO.Modapie/Empleado.cs
+++ DO.Modapie/Empleado.cs
@@ -14,7 +14,7 @@ namespace DO.Modapie
         public string Nombre { get; set; }
 
         public string Apellido1 { get; set; }
-
+C:\Users\ricar\source\repos\NewRepo\ProyectoA2\DO.Modapie\VentaAlxMayor.cs
         public string Apellido2 { get; set; }
 
         public string Celular { get; set; }
---
+++ DO.Modapie/VentaAlxMayor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO.Modapie
+{
+    public class VentaAlxMayor
+    {
+
+        public int IdVentaMayor { get; set; }
+        public int IdClienteMayor{ get; set; }
+        public String IdEmpleado { get; set; }
+        public Double Total { get; set; }
+    }
+}
