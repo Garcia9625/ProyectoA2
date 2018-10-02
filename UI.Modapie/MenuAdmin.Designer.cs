773a817f5a25e719d28bd87c2260ed9f1e5666e7
Ricardo <ricardogarcia9625@gmail.com>
2018-10-02 11:45:30 -0600

    cambios menu e informes

--- UI.Modapie/MenuAdmin.Designer.cs
+++ UI.Modapie/MenuAdmin.Designer.cs
@@ -38,16 +38,16 @@
             this.button4 = new System.Windows.Forms.Button();
             this.btn_NuevoUsuario = new System.Windows.Forms.Button();
             this.btn_GIAXM = new System.Windows.Forms.Button();
-            this.btnVentasAlXM = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnProductos
             // 
-            this.btnProductos.Location = new System.Drawing.Point(150, 259);
+            this.btnProductos.Location = new System.Drawing.Point(169, 324);
+            this.btnProductos.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnProductos.Name = "btnProductos";
-            this.btnProductos.Size = new System.Drawing.Size(154, 26);
+            this.btnProductos.Size = new System.Drawing.Size(216, 32);
             this.btnProductos.TabIndex = 0;
             this.btnProductos.Text = "Gestion de productos";
             this.btnProductos.UseVisualStyleBackColor = true;
@@ -55,9 +55,10 @@
             // 
             // btnClientesPMa
             // 
-            this.btnClientesPMa.Location = new System.Drawing.Point(150, 159);
+            this.btnClientesPMa.Location = new System.Drawing.Point(169, 199);
+            this.btnClientesPMa.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnClientesPMa.Name = "btnClientesPMa";
-            this.btnClientesPMa.Size = new System.Drawing.Size(154, 26);
+            this.btnClientesPMa.Size = new System.Drawing.Size(173, 32);
             this.btnClientesPMa.TabIndex = 1;
             this.btnClientesPMa.Text = "Clientes al por mayor";
             this.btnClientesPMa.UseVisualStyleBackColor = true;
@@ -65,9 +66,10 @@
             // 
             // btnInformes
             // 
-            this.btnInformes.Location = new System.Drawing.Point(190, 409);
+            this.btnInformes.Location = new System.Drawing.Point(214, 511);
+            this.btnInformes.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnInformes.Name = "btnInformes";
-            this.btnInformes.Size = new System.Drawing.Size(75, 26);
+            this.btnInformes.Size = new System.Drawing.Size(84, 32);
             this.btnInformes.TabIndex = 2;
             this.btnInformes.Text = "Informes";
             this.btnInformes.UseVisualStyleBackColor = true;
@@ -77,16 +79,17 @@
             // 
             this.lblUsername.AutoSize = true;
             this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsername.Location = new System.Drawing.Point(61, 9);
+            this.lblUsername.Location = new System.Drawing.Point(69, 11);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(0, 29);
+            this.lblUsername.Size = new System.Drawing.Size(0, 32);
             this.lblUsername.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(144, 209);
+            this.button1.Location = new System.Drawing.Point(162, 261);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(167, 26);
+            this.button1.Size = new System.Drawing.Size(188, 32);
             this.button1.TabIndex = 4;
             this.button1.Text = "Gestion de empleados";
             this.button1.UseVisualStyleBackColor = true;
@@ -94,9 +97,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(155, 109);
+            this.button2.Location = new System.Drawing.Point(174, 136);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(145, 26);
+            this.button2.Size = new System.Drawing.Size(163, 32);
             this.button2.TabIndex = 5;
             this.button2.Text = "Clientes al detalles";
             this.button2.UseVisualStyleBackColor = true;
@@ -104,9 +108,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(118, 359);
+            this.button3.Location = new System.Drawing.Point(133, 449);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(219, 26);
+            this.button3.Size = new System.Drawing.Size(246, 32);
             this.button3.TabIndex = 6;
             this.button3.Text = "Gestion de ventas al por mayor";
             this.button3.UseVisualStyleBackColor = true;
@@ -114,9 +119,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(128, 509);
+            this.button4.Location = new System.Drawing.Point(144, 636);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(198, 26);
+            this.button4.Size = new System.Drawing.Size(223, 32);
             this.button4.TabIndex = 7;
             this.button4.Text = "Gestion de Ventas al detalle";
             this.button4.UseVisualStyleBackColor = true;
@@ -124,9 +130,10 @@
             // 
             // btn_NuevoUsuario
             // 
-            this.btn_NuevoUsuario.Location = new System.Drawing.Point(138, 459);
+            this.btn_NuevoUsuario.Location = new System.Drawing.Point(155, 574);
+            this.btn_NuevoUsuario.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_NuevoUsuario.Name = "btn_NuevoUsuario";
-            this.btn_NuevoUsuario.Size = new System.Drawing.Size(178, 26);
+            this.btn_NuevoUsuario.Size = new System.Drawing.Size(200, 32);
             this.btn_NuevoUsuario.TabIndex = 8;
             this.btn_NuevoUsuario.Text = "Registrar nuevo usuario";
             this.btn_NuevoUsuario.UseVisualStyleBackColor = true;
@@ -134,29 +141,19 @@
             // 
             // btn_GIAXM
             // 
-            this.btn_GIAXM.Location = new System.Drawing.Point(139, 309);
+            this.btn_GIAXM.Location = new System.Drawing.Point(156, 386);
+            this.btn_GIAXM.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_GIAXM.Name = "btn_GIAXM";
-            this.btn_GIAXM.Size = new System.Drawing.Size(176, 26);
+            this.btn_GIAXM.Size = new System.Drawing.Size(229, 32);
             this.btn_GIAXM.TabIndex = 9;
             this.btn_GIAXM.Text = "Gestion Inv. Al por mayor";
             this.btn_GIAXM.UseVisualStyleBackColor = true;
             this.btn_GIAXM.Click += new System.EventHandler(this.btn_GIAXM_Click);
             // 
-            // btnVentasAlXM
-            // 
-            this.btnVentasAlXM.Location = new System.Drawing.Point(138, 555);
-            this.btnVentasAlXM.Name = "btnVentasAlXM";
-            this.btnVentasAlXM.Size = new System.Drawing.Size(172, 26);
-            this.btnVentasAlXM.TabIndex = 11;
-            this.btnVentasAlXM.Text = "Venta al por mayor";
-            this.btnVentasAlXM.UseVisualStyleBackColor = true;
-            this.btnVentasAlXM.Click += new System.EventHandler(this.btnVentasAlXM_Click);
-            // 
             // panel1
             // 
             this.panel1.BackgroundImage = global::UI.Modapie.Properties.Resources.MODAPIE___copia1;
             this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.btnVentasAlXM);
             this.panel1.Controls.Add(this.btnProductos);
             this.panel1.Controls.Add(this.btn_GIAXM);
             this.panel1.Controls.Add(this.btnClientesPMa);
@@ -165,18 +162,20 @@
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.button3);
-            this.panel1.Location = new System.Drawing.Point(-2, -7);
+            this.panel1.Location = new System.Drawing.Point(-2, -9);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(474, 624);
+            this.panel1.Size = new System.Drawing.Size(533, 780);
             this.panel1.TabIndex = 12;
             // 
             // MenuAdmin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(465, 596);
+            this.ClientSize = new System.Drawing.Size(523, 702);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblUsername);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MenuAdmin";
             this.Text = "MenuAdmin";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MenuAdmin_FormClosed);
@@ -199,7 +198,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button btn_NuevoUsuario;
         private System.Windows.Forms.Button btn_GIAXM;
-        private System.Windows.Forms.Button btnVentasAlXM;
         private System.Windows.Forms.Panel panel1;
     }
 }
