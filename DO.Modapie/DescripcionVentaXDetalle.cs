4d8fb2439e1f80c38acdca99422f963221060c60
luis diego jimenez muñoz <l_diegojimenez@hotmail.com>
2018-10-26 17:21:54 -0600

    ni idea

---
+++ DO.Modapie/DescripcionVentaXDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO.Modapie
+{
+    public class DescripcionVentaXDetalle
+    {
+        public int IdDetalleVentaDetalle { get; set; }
+
+        public int IdVentaDetalle { get; set; }
+
+        public int IdProducto { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public double PrecioUnitario { get; set; }
+
+        public double Total { get; set; }
+    }
+}
