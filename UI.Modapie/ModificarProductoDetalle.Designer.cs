34d6a80567648535f059bcd2566fc792f4e1389a
Ricardo <ricardogarcia9625@gmail.com>
2018-11-04 18:11:29 -0600

    lll

--- UI.Modapie/ModificarProductoDetalle.Designer.cs
+++ UI.Modapie/ModificarProductoDetalle.Designer.cs
@@ -30,12 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModificarProductoDetalle));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btn_Buscar = new System.Windows.Forms.Button();
             this.dgvCAD = new System.Windows.Forms.DataGridView();
             this.btnMostrarCAD = new System.Windows.Forms.Button();
             this.btnEditarproducto = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.btnIngresar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txtCodigo = new System.Windows.Forms.TextBox();
             this.txtTalla = new System.Windows.Forms.TextBox();
@@ -56,12 +54,10 @@
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.panel1.BackgroundImage = global::UI.Modapie.Properties.Resources.MODAPIECopia;
-            this.panel1.Controls.Add(this.btn_Buscar);
             this.panel1.Controls.Add(this.dgvCAD);
             this.panel1.Controls.Add(this.btnMostrarCAD);
             this.panel1.Controls.Add(this.btnEditarproducto);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.btnIngresar);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.txtCodigo);
             this.panel1.Controls.Add(this.txtTalla);
@@ -74,185 +70,170 @@
             this.panel1.Controls.Add(this.txtPrecio);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.txtColor);
-            this.panel1.Location = new System.Drawing.Point(-4, -10);
+            this.panel1.Location = new System.Drawing.Point(-4, -12);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1177, 654);
+            this.panel1.Size = new System.Drawing.Size(1324, 818);
             this.panel1.TabIndex = 0;
-            // 
-            // btn_Buscar
-            // 
-            this.btn_Buscar.Location = new System.Drawing.Point(693, 100);
-            this.btn_Buscar.Margin = new System.Windows.Forms.Padding(4);
-            this.btn_Buscar.Name = "btn_Buscar";
-            this.btn_Buscar.Size = new System.Drawing.Size(100, 28);
-            this.btn_Buscar.TabIndex = 23;
-            this.btn_Buscar.Text = "Buscar";
-            this.btn_Buscar.UseVisualStyleBackColor = true;
             // 
             // dgvCAD
             // 
             this.dgvCAD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCAD.Location = new System.Drawing.Point(25, 381);
-            this.dgvCAD.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvCAD.Location = new System.Drawing.Point(97, 476);
+            this.dgvCAD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvCAD.Name = "dgvCAD";
-            this.dgvCAD.Size = new System.Drawing.Size(944, 204);
+            this.dgvCAD.Size = new System.Drawing.Size(1062, 255);
             this.dgvCAD.TabIndex = 36;
             // 
             // btnMostrarCAD
             // 
-            this.btnMostrarCAD.Location = new System.Drawing.Point(592, 329);
-            this.btnMostrarCAD.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMostrarCAD.Location = new System.Drawing.Point(666, 411);
+            this.btnMostrarCAD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMostrarCAD.Name = "btnMostrarCAD";
-            this.btnMostrarCAD.Size = new System.Drawing.Size(144, 28);
+            this.btnMostrarCAD.Size = new System.Drawing.Size(162, 35);
             this.btnMostrarCAD.TabIndex = 39;
             this.btnMostrarCAD.Text = "Mostrar Producto";
             this.btnMostrarCAD.UseVisualStyleBackColor = true;
             // 
             // btnEditarproducto
             // 
-            this.btnEditarproducto.Location = new System.Drawing.Point(422, 329);
-            this.btnEditarproducto.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEditarproducto.Location = new System.Drawing.Point(338, 411);
+            this.btnEditarproducto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnEditarproducto.Name = "btnEditarproducto";
-            this.btnEditarproducto.Size = new System.Drawing.Size(144, 28);
+            this.btnEditarproducto.Size = new System.Drawing.Size(162, 35);
             this.btnEditarproducto.TabIndex = 38;
             this.btnEditarproducto.Text = "Editar Producto";
             this.btnEditarproducto.UseVisualStyleBackColor = true;
+            this.btnEditarproducto.Click += new System.EventHandler(this.btnEditarproducto_Click);
             // 
             // button1
             // 
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(916, 37);
+            this.button1.Location = new System.Drawing.Point(1030, 46);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(53, 42);
+            this.button1.Size = new System.Drawing.Size(60, 52);
             this.button1.TabIndex = 40;
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // btnIngresar
-            // 
-            this.btnIngresar.Location = new System.Drawing.Point(247, 329);
-            this.btnIngresar.Margin = new System.Windows.Forms.Padding(4);
-            this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(144, 28);
-            this.btnIngresar.TabIndex = 37;
-            this.btnIngresar.Text = "Ingresar Producto";
-            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(364, 103);
+            this.label1.Location = new System.Drawing.Point(93, 182);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.Size = new System.Drawing.Size(63, 20);
             this.label1.TabIndex = 21;
             this.label1.Text = "Código:";
             // 
             // txtCodigo
             // 
-            this.txtCodigo.Location = new System.Drawing.Point(438, 103);
-            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCodigo.Location = new System.Drawing.Point(219, 179);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(225, 22);
+            this.txtCodigo.Size = new System.Drawing.Size(253, 26);
             this.txtCodigo.TabIndex = 22;
             // 
             // txtTalla
             // 
-            this.txtTalla.Location = new System.Drawing.Point(232, 261);
-            this.txtTalla.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTalla.Location = new System.Drawing.Point(219, 323);
+            this.txtTalla.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtTalla.Name = "txtTalla";
-            this.txtTalla.Size = new System.Drawing.Size(225, 22);
+            this.txtTalla.Size = new System.Drawing.Size(253, 26);
             this.txtTalla.TabIndex = 31;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(83, 182);
+            this.label2.Location = new System.Drawing.Point(93, 228);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.Size = new System.Drawing.Size(117, 20);
             this.label2.TabIndex = 24;
             this.label2.Text = "Precio unitario: ";
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(645, 264);
-            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCantidad.Location = new System.Drawing.Point(726, 330);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(264, 22);
+            this.txtCantidad.Size = new System.Drawing.Size(296, 26);
             this.txtCantidad.TabIndex = 34;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(83, 223);
+            this.label3.Location = new System.Drawing.Point(93, 279);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.Size = new System.Drawing.Size(54, 20);
             this.label3.TabIndex = 25;
             this.label3.Text = "Color: ";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(544, 263);
+            this.label8.Location = new System.Drawing.Point(612, 329);
             this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(72, 17);
+            this.label8.Size = new System.Drawing.Size(81, 20);
             this.label8.TabIndex = 33;
             this.label8.Text = "Cantidad: ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(83, 264);
+            this.label4.Location = new System.Drawing.Point(93, 330);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.Size = new System.Drawing.Size(50, 20);
             this.label4.TabIndex = 26;
             this.label4.Text = "Talla: ";
             // 
             // txtDescripcion
             // 
-            this.txtDescripcion.Location = new System.Drawing.Point(645, 177);
-            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDescripcion.Location = new System.Drawing.Point(726, 221);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtDescripcion.Multiline = true;
             this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new System.Drawing.Size(264, 79);
+            this.txtDescripcion.Size = new System.Drawing.Size(296, 98);
             this.txtDescripcion.TabIndex = 32;
             // 
             // txtPrecio
             // 
-            this.txtPrecio.Location = new System.Drawing.Point(232, 177);
-            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPrecio.Location = new System.Drawing.Point(218, 221);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtPrecio.Name = "txtPrecio";
-            this.txtPrecio.Size = new System.Drawing.Size(225, 22);
+            this.txtPrecio.Size = new System.Drawing.Size(253, 26);
             this.txtPrecio.TabIndex = 29;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(546, 182);
+            this.label5.Location = new System.Drawing.Point(614, 228);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 17);
+            this.label5.Size = new System.Drawing.Size(96, 20);
             this.label5.TabIndex = 27;
             this.label5.Text = "Descripción:";
             // 
             // txtColor
             // 
-            this.txtColor.Location = new System.Drawing.Point(232, 220);
-            this.txtColor.Margin = new System.Windows.Forms.Padding(4);
+            this.txtColor.Location = new System.Drawing.Point(218, 273);
+            this.txtColor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtColor.Name = "txtColor";
-            this.txtColor.Size = new System.Drawing.Size(225, 22);
+            this.txtColor.Size = new System.Drawing.Size(253, 26);
             this.txtColor.TabIndex = 30;
             // 
-            // ProductoDetalle
+            // ModificarProductoDetalle
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1167, 639);
+            this.ClientSize = new System.Drawing.Size(1313, 799);
             this.Controls.Add(this.panel1);
-            this.Name = "ProductoDetalle";
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "ModificarProductoDetalle";
             this.Text = "ProductoDetalle";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -264,12 +245,10 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btn_Buscar;
         private System.Windows.Forms.DataGridView dgvCAD;
         private System.Windows.Forms.Button btnMostrarCAD;
         private System.Windows.Forms.Button btnEditarproducto;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtCodigo;
         private System.Windows.Forms.TextBox txtTalla;
--- UI.Modapie/ModificarProductoDetalle.cs
+++ UI.Modapie/ModificarProductoDetalle.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BS.Modapie;
+using DO.Modapie;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +9,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace UI.Modapie
 {
     public partial class ModificarProductoDetalle : Form
     {
+
+        Mantenimiento procesar = new Mantenimiento();
+        DO.Modapie.ProductoDetalle IAD;
+
+
+
         public ModificarProductoDetalle(string cod,double prec,string color, double talla, string desc, int canti)
         {
             InitializeComponent();
@@ -22,9 +29,47 @@ namespace UI.Modapie
             txtDescripcion.Text = desc;
             txtCantidad.Text = canti.ToString();
         }
-
-        private void btnIngresar_Click(object sender, EventArgs e)
+        private void GetValues()
         {
+            IAD= new DO.Modapie.ProductoDetalle
+            {
+                
+
+                Codigo = txtCodigo.Text,
+                PrecioUnitario = Convert.ToInt32(txtPrecio.Text),
+                Color= txtColor.Text,
+                Talla= Convert.ToInt32(txtTalla.Text),
+                Cantidad= Convert.ToInt32(txtCantidad.Text),
+                Descripcion= txtDescripcion.Text
+
+
+
+
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            GestionProductosAlDetalle gpd = new GestionProductosAlDetalle();
+            gpd.Show();
+
+        }
+
+        private void btnEditarproducto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GetValues();
+                Mantenimiento.Instancia.EditarproductoDetalle(IAD);
+                dgvCAD.DataSource = procesar.MostrarproductoDetalle();
+                MessageBox.Show("Se edito correctamente el producto", "Editar Producto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ee)
+            {
+                throw;
+                //MessageBox.Show("No se pudo editar el producto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
 
         }
     }
