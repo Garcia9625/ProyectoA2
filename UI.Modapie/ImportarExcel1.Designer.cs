cf022ebe46a9390eda6bd6e8bc392feef43e3e9e
Esteban Vargas G <estebanvg96@hotmail.com>
2018-12-08 21:04:35 -0600

    Correcciones en cliente al por mayor, gestion de empleado, importacion de excel

--- UI.Modapie/ImportarExcel1.Designer.cs
+++ UI.Modapie/ImportarExcel1.Designer.cs
@@ -33,6 +33,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.BTN_ImportExcel = new System.Windows.Forms.Button();
             this.btn_Guardar = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBuscarArchivo = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -40,33 +42,37 @@
             // panel1
             // 
             this.panel1.BackgroundImage = global::UI.Modapie.Properties.Resources.MODAPIECopia1;
+            this.panel1.Controls.Add(this.btnBuscarArchivo);
+            this.panel1.Controls.Add(this.txtPath);
             this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.BTN_ImportExcel);
             this.panel1.Controls.Add(this.btn_Guardar);
-            this.panel1.Location = new System.Drawing.Point(-4, -6);
+            this.panel1.Location = new System.Drawing.Point(-3, -5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1104, 676);
+            this.panel1.Size = new System.Drawing.Size(828, 549);
             this.panel1.TabIndex = 46;
             // 
             // dataGridView1
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(25, 106);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(19, 86);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1050, 372);
+            this.dataGridView1.Size = new System.Drawing.Size(788, 302);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Image = global::UI.Modapie.Properties.Resources.UndoRed48x48;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(956, 18);
+            this.button1.Location = new System.Drawing.Point(717, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 52);
+            this.button1.Size = new System.Drawing.Size(98, 42);
             this.button1.TabIndex = 45;
             this.button1.Text = "Regresar";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -77,10 +83,10 @@
             // 
             this.BTN_ImportExcel.Image = global::UI.Modapie.Properties.Resources.Arrow_Up_48x48;
             this.BTN_ImportExcel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.BTN_ImportExcel.Location = new System.Drawing.Point(850, 498);
-            this.BTN_ImportExcel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BTN_ImportExcel.Location = new System.Drawing.Point(638, 405);
+            this.BTN_ImportExcel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BTN_ImportExcel.Name = "BTN_ImportExcel";
-            this.BTN_ImportExcel.Size = new System.Drawing.Size(212, 59);
+            this.BTN_ImportExcel.Size = new System.Drawing.Size(159, 48);
             this.BTN_ImportExcel.TabIndex = 1;
             this.BTN_ImportExcel.Text = "Importar";
             this.BTN_ImportExcel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -91,28 +97,46 @@
             // 
             this.btn_Guardar.Image = global::UI.Modapie.Properties.Resources.guardar;
             this.btn_Guardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_Guardar.Location = new System.Drawing.Point(855, 582);
-            this.btn_Guardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Guardar.Location = new System.Drawing.Point(641, 473);
+            this.btn_Guardar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Guardar.Name = "btn_Guardar";
-            this.btn_Guardar.Size = new System.Drawing.Size(206, 62);
+            this.btn_Guardar.Size = new System.Drawing.Size(154, 50);
             this.btn_Guardar.TabIndex = 2;
             this.btn_Guardar.Text = "Guardar";
             this.btn_Guardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_Guardar.UseVisualStyleBackColor = true;
             this.btn_Guardar.Click += new System.EventHandler(this.btn_Guardar_Click);
             // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(46, 417);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(100, 20);
+            this.txtPath.TabIndex = 46;
+            // 
+            // btnBuscarArchivo
+            // 
+            this.btnBuscarArchivo.Location = new System.Drawing.Point(339, 418);
+            this.btnBuscarArchivo.Name = "btnBuscarArchivo";
+            this.btnBuscarArchivo.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscarArchivo.TabIndex = 47;
+            this.btnBuscarArchivo.Text = "Buscar";
+            this.btnBuscarArchivo.UseVisualStyleBackColor = true;
+            this.btnBuscarArchivo.Click += new System.EventHandler(this.btnBuscarArchivo_Click);
+            // 
             // ImportarExcel1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1094, 664);
+            this.ClientSize = new System.Drawing.Size(820, 540);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ImportarExcel1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.ImportarExcel1_Load);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -125,5 +149,7 @@
         private System.Windows.Forms.Button btn_Guardar;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBuscarArchivo;
+        private System.Windows.Forms.TextBox txtPath;
     }
 }
